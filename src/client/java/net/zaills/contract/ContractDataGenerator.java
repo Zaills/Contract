5e3a6c35874c84685b6ea60af4f2cff8381481e7
Zaills <zelephraph@gmail.com>
2026-04-11 00:29:23 +0200

    add contract texture

--- src/client/java/net/zaills/contract/ContractDataGenerator.java
+++ src/client/java/net/zaills/contract/ContractDataGenerator.java
@@ -2,10 +2,13 @@ package net.zaills.contract;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.zaills.contract.datagen.ContractModelProvider;
 
 public class ContractDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(ContractModelProvider::new);
 
 	}
 }
